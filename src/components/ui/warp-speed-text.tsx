ca753dca543d522217824fd8351515a4ced5670b
Henrique Faria <henriqueplay73@hotmail.com>
2025-07-04 02:25:38 -0300

    fix: atualizar WarpSpeedText com valores exatos dos gradientes SVG conforme especificado

--- src/components/ui/warp-speed-text.tsx
+++ src/components/ui/warp-speed-text.tsx
@@ -11,9 +11,9 @@ export function WarpSpeedText({ children, className = '' }: WarpSpeedTextProps) 
     <div className={`bg-grid-small-neutral-200 hover:bg-grid-small-neutral-800 dark:bg-grid-small-neutral-800 group relative inline-block rounded-sm bg-neutral-100 px-2 py-2 transition duration-200 hover:bg-neutral-900 dark:bg-neutral-900 ${className}`}>
       {/* SVG Gradients - Top */}
       <svg width="600" height="1" viewBox="0 0 600 1" fill="none" xmlns="http://www.w3.org/2000/svg" className="left-0 w-full absolute top-2">
-        <path d="M0 0.5H800" stroke="url(#svgGradient-r6c)"></path>
+        <path d="M0 0.5H800" stroke="url(#svgGradient-rm)"></path>
         <defs>
-          <linearGradient id="svgGradient-r6c" gradientUnits="userSpaceOnUse" x1="0.66%" x2="-4.34%" y1="0" y2="0">
+          <linearGradient id="svgGradient-rm" gradientUnits="userSpaceOnUse" x1="110%" x2="105%" y1="0" y2="0">
             <stop stopColor="#2EB9DF" stopOpacity="0"></stop>
             <stop stopColor="var(--color-blue-500)"></stop>
             <stop offset="1" stopColor="var(--color-blue-500)" stopOpacity="0"></stop>
@@ -22,9 +22,9 @@ export function WarpSpeedText({ children, className = '' }: WarpSpeedTextProps) 
       </svg>
       
       <svg width="600" height="1" viewBox="0 0 600 1" fill="none" xmlns="http://www.w3.org/2000/svg" className="left-0 w-full absolute top-4">
-        <path d="M0 0.5H800" stroke="url(#svgGradient-r6d)"></path>
+        <path d="M0 0.5H800" stroke="url(#svgGradient-rn)"></path>
         <defs>
-          <linearGradient id="svgGradient-r6d" gradientUnits="userSpaceOnUse" x1="0.33%" x2="-4.67%" y1="0" y2="0">
+          <linearGradient id="svgGradient-rn" gradientUnits="userSpaceOnUse" x1="81.565%" x2="76.565%" y1="0" y2="0">
             <stop stopColor="#2EB9DF" stopOpacity="0"></stop>
             <stop stopColor="var(--color-blue-500)"></stop>
             <stop offset="1" stopColor="var(--color-blue-500)" stopOpacity="0"></stop>
@@ -33,9 +33,9 @@ export function WarpSpeedText({ children, className = '' }: WarpSpeedTextProps) 
       </svg>
       
       <svg width="600" height="1" viewBox="0 0 600 1" fill="none" xmlns="http://www.w3.org/2000/svg" className="left-0 w-full absolute top-6">
-        <path d="M0 0.5H800" stroke="url(#svgGradient-r6e)"></path>
+        <path d="M0 0.5H800" stroke="url(#svgGradient-ro)"></path>
         <defs>
-          <linearGradient id="svgGradient-r6e" gradientUnits="userSpaceOnUse" x1="36.88667%" x2="31.88667%" y1="0" y2="0">
+          <linearGradient id="svgGradient-ro" gradientUnits="userSpaceOnUse" x1="72.71%" x2="67.71%" y1="0" y2="0">
             <stop stopColor="#2EB9DF" stopOpacity="0"></stop>
             <stop stopColor="var(--color-blue-500)"></stop>
             <stop offset="1" stopColor="var(--color-blue-500)" stopOpacity="0"></stop>
@@ -44,9 +44,9 @@ export function WarpSpeedText({ children, className = '' }: WarpSpeedTextProps) 
       </svg>
       
       <svg width="600" height="1" viewBox="0 0 600 1" fill="none" xmlns="http://www.w3.org/2000/svg" className="left-0 w-full absolute top-8">
-        <path d="M0 0.5H800" stroke="url(#svgGradient-r6f)"></path>
+        <path d="M0 0.5H800" stroke="url(#svgGradient-rp)"></path>
         <defs>
-          <linearGradient id="svgGradient-r6f" gradientUnits="userSpaceOnUse" x1="0.165%" x2="-4.835%" y1="0" y2="0">
+          <linearGradient id="svgGradient-rp" gradientUnits="userSpaceOnUse" x1="40.7825%" x2="35.7825%" y1="0" y2="0">
             <stop stopColor="#2EB9DF" stopOpacity="0"></stop>
             <stop stopColor="var(--color-blue-500)"></stop>
             <stop offset="1" stopColor="var(--color-blue-500)" stopOpacity="0"></stop>
@@ -55,9 +55,9 @@ export function WarpSpeedText({ children, className = '' }: WarpSpeedTextProps) 
       </svg>
       
       <svg width="600" height="1" viewBox="0 0 600 1" fill="none" xmlns="http://www.w3.org/2000/svg" className="left-0 w-full absolute top-10">
-        <path d="M0 0.5H800" stroke="url(#svgGradient-r6g)"></path>
+        <path d="M0 0.5H800" stroke="url(#svgGradient-rq)"></path>
         <defs>
-          <linearGradient id="svgGradient-r6g" gradientUnits="userSpaceOnUse" x1="110%" x2="105%" y1="0" y2="0">
+          <linearGradient id="svgGradient-rq" gradientUnits="userSpaceOnUse" x1="81.565%" x2="76.565%" y1="0" y2="0">
             <stop stopColor="#2EB9DF" stopOpacity="0"></stop>
             <stop stopColor="var(--color-blue-500)"></stop>
             <stop offset="1" stopColor="var(--color-blue-500)" stopOpacity="0"></stop>
@@ -67,9 +67,9 @@ export function WarpSpeedText({ children, className = '' }: WarpSpeedTextProps) 
       
       {/* SVG Gradients - Bottom */}
       <svg width="600" height="1" viewBox="0 0 600 1" fill="none" xmlns="http://www.w3.org/2000/svg" className="left-0 w-full absolute bottom-2">
-        <path d="M0 0.5H800" stroke="url(#svgGradient-r6h)"></path>
+        <path d="M0 0.5H800" stroke="url(#svgGradient-rr)"></path>
         <defs>
-          <linearGradient id="svgGradient-r6h" gradientUnits="userSpaceOnUse" x1="0.66%" x2="-4.34%" y1="0" y2="0">
+          <linearGradient id="svgGradient-rr" gradientUnits="userSpaceOnUse" x1="110%" x2="105%" y1="0" y2="0">
             <stop stopColor="#2EB9DF" stopOpacity="0"></stop>
             <stop stopColor="var(--color-blue-500)"></stop>
             <stop offset="1" stopColor="var(--color-blue-500)" stopOpacity="0"></stop>
@@ -78,9 +78,9 @@ export function WarpSpeedText({ children, className = '' }: WarpSpeedTextProps) 
       </svg>
       
       <svg width="600" height="1" viewBox="0 0 600 1" fill="none" xmlns="http://www.w3.org/2000/svg" className="left-0 w-full absolute bottom-4">
-        <path d="M0 0.5H800" stroke="url(#svgGradient-r6i)"></path>
+        <path d="M0 0.5H800" stroke="url(#svgGradient-rs)"></path>
         <defs>
-          <linearGradient id="svgGradient-r6i" gradientUnits="userSpaceOnUse" x1="110%" x2="105%" y1="0" y2="0">
+          <linearGradient id="svgGradient-rs" gradientUnits="userSpaceOnUse" x1="81.565%" x2="76.565%" y1="0" y2="0">
             <stop stopColor="#2EB9DF" stopOpacity="0"></stop>
             <stop stopColor="var(--color-blue-500)"></stop>
             <stop offset="1" stopColor="var(--color-blue-500)" stopOpacity="0"></stop>
@@ -89,9 +89,9 @@ export function WarpSpeedText({ children, className = '' }: WarpSpeedTextProps) 
       </svg>
       
       <svg width="600" height="1" viewBox="0 0 600 1" fill="none" xmlns="http://www.w3.org/2000/svg" className="left-0 w-full absolute bottom-6">
-        <path d="M0 0.5H800" stroke="url(#svgGradient-r6j)"></path>
+        <path d="M0 0.5H800" stroke="url(#svgGradient-rt)"></path>
         <defs>
-          <linearGradient id="svgGradient-r6j" gradientUnits="userSpaceOnUse" x1="0.165%" x2="-4.835%" y1="0" y2="0">
+          <linearGradient id="svgGradient-rt" gradientUnits="userSpaceOnUse" x1="40.7825%" x2="35.7825%" y1="0" y2="0">
             <stop stopColor="#2EB9DF" stopOpacity="0"></stop>
             <stop stopColor="var(--color-blue-500)"></stop>
             <stop offset="1" stopColor="var(--color-blue-500)" stopOpacity="0"></stop>
@@ -100,9 +100,9 @@ export function WarpSpeedText({ children, className = '' }: WarpSpeedTextProps) 
       </svg>
       
       <svg width="600" height="1" viewBox="0 0 600 1" fill="none" xmlns="http://www.w3.org/2000/svg" className="left-0 w-full absolute bottom-8">
-        <path d="M0 0.5H800" stroke="url(#svgGradient-r6k)"></path>
+        <path d="M0 0.5H800" stroke="url(#svgGradient-ru)"></path>
         <defs>
-          <linearGradient id="svgGradient-r6k" gradientUnits="userSpaceOnUse" x1="110%" x2="105%" y1="0" y2="0">
+          <linearGradient id="svgGradient-ru" gradientUnits="userSpaceOnUse" x1="110%" x2="105%" y1="0" y2="0">
             <stop stopColor="#2EB9DF" stopOpacity="0"></stop>
             <stop stopColor="var(--color-blue-500)"></stop>
             <stop offset="1" stopColor="var(--color-blue-500)" stopOpacity="0"></stop>
